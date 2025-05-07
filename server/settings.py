f22eb63dc6eb975eb12cb916ab39b3ae08d8a69d
tanjim750 <mdtanjim750@gmail.com>
2025-05-07 23:26:30 +0600

    wip

--- server/settings.py
+++ server/settings.py
@@ -143,3 +143,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
