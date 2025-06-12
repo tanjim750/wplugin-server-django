5e65c7c29c5a24527f43090c7d2f14b5caaff8b2
tanjim750 <mdtanjim750@gmail.com>
2025-06-12 20:24:22 +0600

    added requirements.txt

--- server/settings.py
+++ server/settings.py
@@ -137,11 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
