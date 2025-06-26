84263a8cad80a81c10e82c1e422dfd13ff4fd2a1
tanjim750 <mdtanjim750@gmail.com>
2025-06-27 01:36:34 +0600

    cors

--- server/settings.py
+++ server/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
